884142ee9ba04b918ba94fa9e5541f2daa1c8aa6
Andrey Khudyakov <avh_2004@mail.ru>
2022-10-20 14:40:45 +0500

    Added Exersize №23 Cubes from 1 to N

---
+++ Ex023_Cube1toN/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// Примеры:
+// 3 -> 1, 8, 27 
+// 5 -> 1, 8, 27, 64, 125
+
+int AskAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int N = AskAndGetValue("Введите целое число:");
+
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if (i < N)
+        {
+            Console.Write(Math.Pow(i, 3) + ", ");
+        }
+        else Console.Write(Math.Pow(i, 3));
+    }
+}
+else
+{
+    for (int i = N; i < 0; i++)
+    {
+        if (i < -1)
+        {
+            Console.Write(Math.Pow(i, 3) + ", ");
+        }
+        else Console.Write(Math.Pow(i, 3));
+    }
+}
